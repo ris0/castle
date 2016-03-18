f382073e2f03559db70f799dd6e67cd80a3ae056
Chris de Villiers <cpdevill@gmail.com>
2016-03-18 12:39:18 -0400

    Basic grid on index.html, inline script needs to be moved to a directive and linked to our interact.js grid variables

--- browser/js/gridtesting/gridtesting.js
+++ browser/js/gridtesting/gridtesting.js
@@ -1,17 +1,12 @@
-app.config(function ($stateProvider) {
+// app.config(function ($stateProvider) {
 
-    // Register our *gridtesting* state.
-    $stateProvider.state('gridtesting', {
-        url: '/gridtesting',
-        controller: 'AboutController',
-        templateUrl: 'js/gridtesting/gridtesting.html'
-    });
+//     // Register our *about* state.
+//     $stateProvider.state('gridtesting', {
+//         url: '/gridtesting',
+//         controller: 'GridTestingCtrl',
+//         templateUrl: 'js/gridtesting/gridtesting.html'
+//     });
 
-});
+// });
 
-app.controller('AboutController', function ($scope, FullstackPics) {
-
-    // Images of beautiful Fullstack people.
-    $scope.images = _.shuffle(FullstackPics);
-
-});
+// app.controller('GridTestingCtrl', function () {});
