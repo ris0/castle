a1efd8e235c1a6af58b79d6e3eb3e8fd4fe7991e
johnquiwa <jcq211@nyu.edu>
2016-03-21 19:49:36 -0400

    cleaned up lobby code

--- browser/js/gameState/gameStateController.js
+++ browser/js/gameState/gameStateController.js
@@ -1,51 +1,46 @@
-app.controller('gameStats', function($scope, $firebaseObject, gameFactory) {
-  var userID = gameFactory.auth().$getAuth().uid
-  console.log(userID);
-  var userGame = $firebaseObject(gameFactory.ref().child('users').child(userID).child('game'));
-  console.log(userGame.child($value));
-  var syncObject = $firebaseObject(gameFactory.ref().child('games'));
-  console.log(syncObject);
-  syncObject.$bindTo($scope, "data");
+app.controller('gameStats', function(syncObject, $scope, $firebaseObject, gameFactory) {
 
-	$scope.drawToMarket = function(){
-		$scope.data.marketQ = [];
+syncObject.$bindTo($scope, "data")
+	.then(function(){
+	  	console.log($scope.data);
 
-		var x = _.countBy($scope.data.market, function(el, key){
-			return (el === 'empty') ? key: false;
-		});
+		$scope.drawToMarket = function(){
+			$scope.data.marketQ = [];
 
-		var full = x.false || 0;
-		console.log(full);
+			var x = _.countBy($scope.data.market, function(el, key){
+				return (el === 'empty') ? key: false;
+			});
 
-		for(var i = 0; $scope.data.marketQ.length < 7-full; i++){
-			//remove from roomCards & roomTiles
-			var nextTile = $scope.data.roomTiles[$scope.data.roomCards[i]][0];
-			if(nextTile){
-				$scope.data.marketQ.push(nextTile);
+			var full = x.false || 0;
+			console.log(full);
+
+			for(var i = 0; $scope.data.marketQ.length < 7-full; i++){
+				//remove from roomCards & roomTiles
+				var nextTile = $scope.data.roomTiles[$scope.data.roomCards[i]][0];
+				if(nextTile){
+					$scope.data.marketQ.push(nextTile);
+				}
 			}
-		}
-	};
+		};
 
-	$scope.assignPrices = function(){
-		//how to assign w/ dragging?
-		var i = 0;
-		for (var price in $scope.data.market){
-			if($scope.data.market[price] === 'empty'){
-				$scope.data.market[price] = $scope.data.marketQ[i];
-				i++;
+		$scope.assignPrices = function(){
+			//how to assign w/ dragging?
+			var i = 0;
+			for (var price in $scope.data.market){
+				if($scope.data.market[price] === 'empty'){
+					$scope.data.market[price] = $scope.data.marketQ[i];
+					i++;
+				}
 			}
-		}
 
-		$scope.data.marketQ = [];
-	};
+			$scope.data.marketQ = [];
+		};
 
-	$scope.buyRoom = function(room, price){
-		//give money to master builder
-		$scope.data.market[price] = "empty";
-		// $scope.data.players[0].castle.push(room);
-	};
-
-	console.log(gameFactory.auth());
-	
-
+		$scope.buyRoom = function(room, price){
+			//give money to master builder
+			$scope.data.market[price] = "empty";
+			// $scope.data.players[0].castle.push(room);
+		};
+		
+	})
 });
--- browser/js/lobby/lobby.js
+++ browser/js/lobby/lobby.js
@@ -1,106 +1,77 @@
-//show login/register page
+app.controller('LobbyCtrl', function (usersRef, userEmail, userId, gamesRef, playersRef, baseStateRef, syncObject, $scope, $state, $firebaseArray, $firebaseObject, gameFactory) {
 
-//if user is logged in show lobby
-
-	//random match
-		//create player queue => playerArr of four userID's
-		//set timer (setTimout) of 4 minutes to find another player
-		//playerArr must have a length of at LEAST 2 userID's to push game
-			//if playerArr.length >= 2
-				//create new firebase gameState object with only those players
-				//$state.go
-
-	//find a certain game by gameID
-		//people can create games and send email invitations with gameID?
-		//enter gameID to be added to playerArr
-		//in view a button appears to $state.go to that game;
-
-	//chat function?
-
-
-//user authentication w/ firebase
-
-app.config(function ($stateProvider) {
-
-    $stateProvider.state('lobby', {
-        url: '/lobby',
-        templateUrl: 'js/lobby/lobby.html',
-        controller: 'LobbyCtrl'
-    });
-
-});
-
-app.controller('LobbyCtrl', function ($scope, $state, $firebaseObject, $firebaseArray, gameFactory) {
-
-  var syncObject = $firebaseObject(gameFactory.ref());
-  syncObject.$bindTo($scope, "data");
   $scope.auth = gameFactory.auth();
-  $scope.baseStateRef = gameFactory.ref().child("baseState");
-  $scope.gamesRef = gameFactory.ref().child("games");
-  $scope.playersRef = gameFactory.ref().child("playersQueue");
-  $scope.usersRef = gameFactory.ref().child("users");
-  $scope.userId = $scope.auth.$getAuth().uid;
-  $scope.userEmail = $scope.auth.$getAuth().password.email;
-	
 
-  $scope.baseStateRef.once('value', function(baseState){
-  	console.log(baseState.val())
-  	$scope.baseState = _.clone(baseState.val());
-  });	
+  var userObj = $firebaseObject(usersRef.child(userId));
+  userObj.$loaded().then(function(user){
+    $scope.user = user;
+  });
+  
+  syncObject.$bindTo($scope, "data").then(function(){
 
-  $scope.goToGame = function(){
-  	$state.go('game');
-  };
 
-  $scope.createGame = function(players){
-  	$scope.playersRef.once('value', function(playersObj){
-  		var players = _.clone(playersObj.val());
-  		console.log(players);
-  		$scope.counter = 0;
-  		for(var key in players){
-  			console.log(players[key]);
-  			$scope.baseState.players[$scope.counter].userID = players[key].userId;
-  			$scope.baseState.players[$scope.counter].userName = players[key].email;
-  			$scope.counter ++;
-  		}
-  		$scope.playersRef.remove();
-  		var newGameRef = $scope.gamesRef.push($scope.baseState);
-  		var gameID = newGameRef.key();
-  		console.log(gameID);
+    baseStateRef.once('value', function(baseState){
+    	$scope.baseState = _.clone(baseState.val());
+    });	
 
-  		$scope.gamesRef.child(gameID).once('value', function(game){
-  			game.val().players.forEach(function(player){
-  				$scope.addGameToUsers(player.userID, gameID);
-  			});
-  		});
-  	});
-  	console.log('removed');
-  }
+    $scope.goToGame = function(){
+    	$state.go('game');
+    };
 
-  	console.log($scope.usersRef);
+    $scope.createGame = function(){
+    	playersRef.once('value', function(playersObj){
+      		var players = _.clone(playersObj.val());
+      		$scope.counter = 0;
+      		for(var key in players){
+      	
+      			$scope.baseState.players[$scope.counter].userID = players[key].userId;
+      			$scope.baseState.players[$scope.counter].userName = players[key].email;
+      			$scope.counter ++;
+      		}
+      		playersRef.remove();
 
-  $scope.addGameToUsers = function(uid, gameID){
-  	$scope.usersRef.child(uid).child('game').set(gameID);
-  };
+      		var newGameRef = gamesRef.push($scope.baseState);
+      		var gameID = newGameRef.key();
+      
+          var gamePlayersArr = $firebaseArray(gamesRef.child(gameID).child("players"));
 
-  $scope.findRandomGame = function(){
+          gamePlayersArr.$loaded().then(function(gamePlayers){
+              for(var j = 0; j < $scope.counter; j++){
+                $scope.addGameToUsers(gamePlayers[j].userID, gameID);
+              }
+              for(var i = $scope.counter+1; i < gamePlayers.length; i++){
+                gamePlayers.$remove(i);
+              }
+          })
+          console.log('Game has been created');
+    	});
+  
+    }
 
-  	if($scope.data.playersQueue){
-  		for(var key in $scope.data.playersQueue){
-  			console.log($scope.userId);
-  			if($scope.data.playersQueue[key] === $scope.userId) {
-  				console.log('User is already in this game!');
-  				console.log($scope.data.playersQueue)
-  				return;
-  			}
-  		}
-  		$scope.playersRef.push({userId : $scope.userId, email : $scope.userEmail})
-  	}else{
-  		$scope.playersRef.push({userId : $scope.userId, email : $scope.userEmail})
-	  	setTimeout($scope.createGame, 10000);
-	  	console.log('10 seconds');
-	 }
-  }
+    $scope.addGameToUsers = function(uid, gameID){
+    	usersRef.child(uid).child('game').set(gameID);
+    };
+
+    $scope.findRandomGame = function(){
+
+    	if($scope.data.playersQueue) {
+
+    		for(var key in $scope.data.playersQueue){
+    			if($scope.data.playersQueue[key] === userId) return;
+    		}
+
+        playersRef.push({userId : userId, email : userEmail})
+
+    	} else {
+    		playersRef.push({userId : userId, email : userEmail})
+  	  	setTimeout($scope.createGame, 5000);
+        console.log('Game will be ready in 5 seconds');
+  	  }
+
+    }
+
+  });
+
 });
 
 
