5c5fd8936145f6f00ba29c5ac5590e3da6704d0b
Cindy Li <il.ydnic@gmail.com>
2016-03-22 13:25:24 -0400

    turn

--- browser/js/gameState/gameStateController.js
+++ browser/js/gameState/gameStateController.js
@@ -1,46 +1,28 @@
 app.controller('gameStats', function(syncObject, $scope, $firebaseObject, gameFactory) {
 
-syncObject.$bindTo($scope, "data")
+	syncObject.$bindTo($scope, "data")
 	.then(function(){
 	  	console.log($scope.data);
 
-		$scope.drawToMarket = function(){
-			$scope.data.marketQ = [];
+		(function getMyIndex() {
+		  var id = gameFactory.auth().$getAuth().uid;
+		  return $scope.data.players.reduce(function(prev, curr, index) {
+		    if (id === curr.userID) {
+		    	$scope.userIndex = index;
+		      return index;
+		    }
+		    return prev;
+		  }, "");
+		})();
 
-			var x = _.countBy($scope.data.market, function(el, key){
-				return (el === 'empty') ? key: false;
-			});
-
-			var full = x.false || 0;
-			console.log(full);
-
-			for(var i = 0; $scope.data.marketQ.length < 7-full; i++){
-				//remove from roomCards & roomTiles
-				var nextTile = $scope.data.roomTiles[$scope.data.roomCards[i]][0];
-				if(nextTile){
-					$scope.data.marketQ.push(nextTile);
-				}
-			}
+		$scope.buy = function(){
+			if($scope.data.currentPlayer === $scope.userIndex) console.log("Buying");
 		};
 
-		$scope.assignPrices = function(){
-			//how to assign w/ dragging?
-			var i = 0;
-			for (var price in $scope.data.market){
-				if($scope.data.market[price] === 'empty'){
-					$scope.data.market[price] = $scope.data.marketQ[i];
-					i++;
-				}
-			}
-
-			$scope.data.marketQ = [];
+		$scope.done = function(){
+			$scope.data.turnCount++;
+			$scope.data.currentPlayer = ($scope.data.turnCount)%2;
+			if($scope.data.turnCount%2 === 0) $scope.data.masterBuilder = ($scope.data.masterBuilder+1)%2;
 		};
-
-		$scope.buyRoom = function(room, price){
-			//give money to master builder
-			$scope.data.market[price] = "empty";
-			// $scope.data.players[0].castle.push(room);
-		};
-		
-	})
+	});
 });
