29af7971daa95736c56ac7e5e9d0b7952fcb8d17
Chris de Villiers <cpdevill@gmail.com>
2016-03-25 12:30:36 -0400

    merge fixes, added repo field to package.json

--- browser/js/gridtesting/gridtesting.js
+++ browser/js/gridtesting/gridtesting.js
@@ -1,4 +1,5 @@
 app.controller('GridTestingCtrl', function($scope) {});
+
 app.directive('gridtesting', function($window) {
     return {
         restrict: "EA",
@@ -27,7 +28,7 @@ app.directive('gridtesting', function($window) {
                 ],
                 "fence": null,
                 "rotation": 180,
-                "notdraggable": false //temp
+                "notdraggable": false, //temp
             }, {
                 "roomName": "Solar",
                 "imagePath": "images/200-solar.png",
@@ -49,8 +50,8 @@ app.directive('gridtesting', function($window) {
                 "rotation": 0,
                 "notdraggable": false, //temp
             }, {
-                "roomName": "Flute Room",
-                "imagePath": "/images/150-fake-room-money-money.png",
+                "roomName": "Flute-Room",
+                "imagePath": "images/150-fake-room-money-money.png",
                 "sqf": 150,
                 "roomType": "Activity",
                 "placementPts": 3,
@@ -73,8 +74,8 @@ app.directive('gridtesting', function($window) {
                 "fence": null,
                 "rotation": 0,
                 "notdraggable": false, //temp
-
             }];
+
             var drag = d3.behavior.drag()
                 .origin(function(d) {
                     return d;
@@ -82,6 +83,7 @@ app.directive('gridtesting', function($window) {
                 .on("dragstart", dragstarted)
                 .on("drag", dragged)
                 .on("dragend", dragended);
+
             var zoom = d3.behavior.zoom()
                 .scaleExtent([.01, 5])
                 .on("zoom", zooming);
@@ -111,6 +113,7 @@ app.directive('gridtesting', function($window) {
                 .attr("y1", -10)
                 .attr("y2", height + 10)
                 .style("stroke", "royalblue");
+
             gameGrid.append("g")
                 .selectAll("line")
                 .data(d3.range(0, 2500))
@@ -150,8 +153,6 @@ app.directive('gridtesting', function($window) {
                 .on("dblclick", rotate);
 
             var roomImages = roomTiles.append("image")
-            roomTiles.enter()
-                .append("image")
                 .attr("xlink:href", function(d) {
                     return d.imagePath;
                 })
@@ -163,12 +164,11 @@ app.directive('gridtesting', function($window) {
                 });
 
             var roomPaths = roomTiles.append("path")
-                .classed("roomTiles", true)
                 .attr("d", function(d) {
                     return d.svgPath;
                 })
-                .style("stroke", "black")
-                .style("stroke-width", "0.5")
+                .style("stroke", "green")
+                .style("stroke-width", "0")
                 .classed("normal", true);
 
             // -------------------------------------------------------------------------------------------
@@ -189,12 +189,14 @@ app.directive('gridtesting', function($window) {
                         return "rotate(" + d.rotation + " " + (snapX + d.containerDim[0] * 10 / 2) + " " + (snapY + d.containerDim[1] * 10 / 2) + "),translate(" + snapX + "," + snapY + ")";
                     });
             }
+
             function dragstarted(d) {
                 if (d.notdraggable !== true) {
                     d3.event.sourceEvent.stopPropagation();
                     d3.select(this).classed("dragging", true);
                 }
             }
+
             function dragged(d) {
                 if (d.notdraggable !== true) {
                     d.boardPosition[0] += d3.event.dx;
@@ -211,19 +213,17 @@ app.directive('gridtesting', function($window) {
             }
 
             function checkPositioning(d) {
-                
                 console.dir(d); // unplaced room
-                console.dir(roomPaths.data()); // array of all room objects in players room array
-                console.log("roomPaths", roomPaths); // array of all room svg paths in players room array
+                console.dir(roomPaths.data()); // array of room objects of each room
+                console.log(roomPaths); // array of DOM elements/svg paths for each room
             }
-
-
 
             function dragended(d) {
                 if (d.notdraggable != true) {
                     d3.select(this).classed("dragging", false);
                 }
             }
+
             function zooming() {
                 if (!d3.event.sourceEvent) return;
                 gameGrid.attr("transform", "translate(" + d3.event.translate[0] % (10 * d3.event.scale) + "," + d3.event.translate[1] % (10 * d3.event.scale) + ")scale(" + d3.event.scale + ")");
