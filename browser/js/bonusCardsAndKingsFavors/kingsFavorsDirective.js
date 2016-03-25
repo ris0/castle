9bb7a62fa510ee55c8d409a2b67acdafc97d25bb
johnquiwa <jcq211@nyu.edu>
2016-03-25 10:00:42 -0400

    kingsFavors and bonus cards directives

--- browser/js/bonusCardsAndKingsFavors/kingsFavorsDirective.js
+++ browser/js/bonusCardsAndKingsFavors/kingsFavorsDirective.js
@@ -2,11 +2,11 @@ app.directive('kingsFavors', function(kingsFavorsFactory) {
   return {
     restrict: 'E',
     scope: {
+      game: "=",
       favors: "="
     },
     templateUrl: './js/bonusCardsAndKingsFavors/kingsFavorsRanking.html',
     link: function(scope) {
-      
     }
   };
 });
