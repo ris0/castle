da4554e03ef5e80436709b66a02a2f77035ca109
Chris de Villiers <cpdevill@gmail.com>
2016-03-25 11:31:19 -0400

    SVG path overlays on room tiles for intersection/collision detection

--- browser/js/gridtesting/gridtesting.js
+++ browser/js/gridtesting/gridtesting.js
@@ -8,50 +8,72 @@ app.directive('gridtesting', function($window) {
             var width = el[0].clientWidth;
             var height = el[0].clientHeight;
 
-            // var dataArray = [
-            //     { url: "foyer.png", x: width / 2, y: height / 2, sqf: 125, rotation: 0, height: 100, width: 100 },
-            //     { url: "100-pantry.png", x: (width / 2) + 100, y: (height / 2), sqf: 125, rotation: 90, height: 100, width: 100 },
-            //     { url: "350-upper-hall.png", x: width / 2, y: (height / 2) + 100, sqf: 125, rotation: 0, height: 100, width: 100, notdraggable: true }
-
-            // ];
-
             var dataArray = [{
                 "roomName": "YellowFoyer",
-                "imagePath": "/images/foyer.png",
+                "imagePath": "images/foyer.png",
                 "sqf": 125,
                 "roomType": "Corridor",
                 "placementPts": 0,
                 "affectedBy": [],
                 "effectPts": 0,
                 "completed": false,
-                "boardPosition": [0, 0],
+                "boardPosition": [100, 100],
                 "containerDim": [6, 6],
                 "roomDim": [2, 2, 2, 2, 2, 2, 2, 2],
+                "svgPath": "M 15 0 L 45 0 L 60 15 L 60 45 L 45 60 L 15 60 L 0 45 L 0 15 Z",
                 "doors": [
                     [-2, 0],
                     [0, 2],
                     [2, 0]
                 ],
                 "fence": null,
-                "rotation": 0
+                "rotation": 180,
+                "notdraggable": false, //temp
             }, {
                 "roomName": "Solar",
-                "imagePath": "/images/200-solar.png",
+                "imagePath": "images/200-solar.png",
                 "sqf": 200,
                 "roomType": "Sleeping",
                 "placementPts": 2,
                 "affectedBy": ["Outdoor"],
                 "effectPts": 2,
                 "completed": false,
-                "boardPosition": [0, 0],
-                "containerDim": [4, 8],
-                "roomDim": [4, 8],
+                "boardPosition": [60, 60],
+                "containerDim": [8, 4],
+                "roomDim": [8, 4],
+                "svgPath": "M 0 0 L 80 0 L 80 40 L 0 40 Z",
                 "doors": [
                     [4, -1],
                     [3, -2]
                 ],
                 "fence": null,
-                "rotation": 0
+                "rotation": 0,
+                "notdraggable": false, //temp
+            }, {
+                "roomName": "Flute-Room",
+                "imagePath": "images/150-fake-room-money-money.png",
+                "sqf": 150,
+                "roomType": "Activity",
+                "placementPts": 3,
+                "affectedBy": ["Living", "Sleep"],
+                "effectPts": -1,
+                "completed": false,
+                "boardPosition": [160, 100],
+                "containerDim": [6, 6],
+                "roomDim": {
+                    "cx": 0,
+                    "cy": 0,
+                    "r": 3
+                },
+                "svgPath": "M0 30 a30 30 0 1 0 60 0 a30 30 0 1 0 -60 0",
+                "doors": [
+                    [-3, 0],
+                    [0, 3],
+                    [3, 0]
+                ],
+                "fence": null,
+                "rotation": 0,
+                "notdraggable": false, //temp
             }];
 
             var drag = d3.behavior.drag()
@@ -63,8 +85,10 @@ app.directive('gridtesting', function($window) {
                 .on("dragend", dragended);
 
             var zoom = d3.behavior.zoom()
-                .scaleExtent([1, 5])
+                .scaleExtent([.01, 5])
                 .on("zoom", zooming);
+
+            // creating the underlying grid
 
             var svg = d3.select("#gameboard")
                 .append("svg")
@@ -73,15 +97,7 @@ app.directive('gridtesting', function($window) {
                 .call(zoom)
                 .attr("preserveAspectRatio", "xMinYMin meet");
 
-
             var gameGrid = svg.append("g");
-
-            var castle = svg.append("g")
-                .attr("id", "tile");
-
-            var roomTiles = d3.select("#tile")
-                .selectAll("image")
-                .data(dataArray);
 
             gameGrid.append("g")
                 .selectAll("line")
@@ -113,8 +129,30 @@ app.directive('gridtesting', function($window) {
                 })
                 .style("stroke", "royalblue");
 
-            roomTiles.enter()
-                .append("image")
+            // -------------------------------------------------------------------------------------------
+            // -------------------------------------------------------------------------------------------
+            // -------------------------------------------------------------------------------------------
+            // -------------------------------------------------------------------------------------------
+            // -------------------------------------------------------------------------------------------
+
+            // adding tiles to the board
+
+            var castle = svg.append("g")
+                .attr("id", "castle");
+
+            var roomTiles = castle.selectAll("g")
+                .data(dataArray)
+                .enter()
+                .append("g")
+                .attr("transform", function(d) {
+                    var snapX = Math.round(d.boardPosition[0] / 10) * 10;
+                    var snapY = Math.round(d.boardPosition[1] / 10) * 10;
+                    return "rotate(" + d.rotation + " " + (snapX + d.containerDim[0] * 10 / 2) + " " + (snapY + d.containerDim[1] * 10 / 2) + "),translate(" + snapX + "," + snapY + ")";
+                })
+                .call(drag)
+                .on("dblclick", rotate);
+
+            var roomImages = roomTiles.append("image")
                 .attr("xlink:href", function(d) {
                     return d.imagePath;
                 })
@@ -123,25 +161,31 @@ app.directive('gridtesting', function($window) {
                 })
                 .attr("width", function(d) {
                     return d.containerDim[0] * 10;
+                });
+
+            var roomPaths = roomTiles.append("path")
+                .attr("d", function(d) {
+                    return d.svgPath;
                 })
-                .attr("transform", function(d) {
-                    console.log(d);
-                    var snapX = Math.round(d.boardPosition[0] / 10) * 10;
-                    var snapY = Math.round(d.boardPosition[1] / 10) * 10;
-                    return "rotate(" + d.rotation + " " + (snapX + d.containerDim[0] * 10 / 2) + " " + (snapY + d.containerDim[1] * 10 / 2) + "),translate(" + snapX + "," + snapY + ")";
-                })
-                .classed("roomTiles", true)
-                .call(drag)
-                .on("dblclick", rotate);
+                .style("stroke", "green")
+                .style("stroke-width", "0")
+                .classed("normal", true);
+
+            // -------------------------------------------------------------------------------------------
+            // -------------------------------------------------------------------------------------------
+            // -------------------------------------------------------------------------------------------
+            // -------------------------------------------------------------------------------------------
+            // -------------------------------------------------------------------------------------------
+
+            // functions
+
+
 
             function rotate(d) {
-                console.log(this);
                 d3.event.stopPropagation();
                 d3.select(this)
                     .attr("transform", function(d) {
-                        console.log(d);
                         d.rotation = d.rotation + 90;
-                        console.log(d);
                         var snapX = Math.round(d.boardPosition[0] / 10) * 10;
                         var snapY = Math.round(d.boardPosition[1] / 10) * 10;
                         return "rotate(" + d.rotation + " " + (snapX + d.containerDim[0] * 10 / 2) + " " + (snapY + d.containerDim[1] * 10 / 2) + "),translate(" + snapX + "," + snapY + ")";
@@ -156,23 +200,29 @@ app.directive('gridtesting', function($window) {
             }
 
             function dragged(d) {
-                if (d.notdraggable != true) {
-                    if (d.sqf === 500 || d.sqf === 150) {
-                        // circle stuff
-                    } else {
-                        d.boardPosition[0] += d3.event.dx;
-                        d.boardPosition[1] += d3.event.dy;
-                        var snapX = Math.round(d.boardPosition[0] / 10) * 10;
-                        var snapY = Math.round(d.boardPosition[1] / 10) * 10;
-                        d3.select(this).attr("transform", "rotate(" + d.rotation + " " + (snapX + d.containerDim[0] * 10 / 2) + " " + (snapY + d.containerDim[1] * 10 / 2) + "),translate(" + snapX + "," + snapY + ")");
-                    }
+                if (d.notdraggable !== true) {
+                    d.boardPosition[0] += d3.event.dx;
+                    d.boardPosition[1] += d3.event.dy;
+                    var snapX = Math.round(d.boardPosition[0] / 10) * 10;
+                    var snapY = Math.round(d.boardPosition[1] / 10) * 10;
+                    d3.select(this)
+                        .attr("transform", "rotate(" + d.rotation + " " + (snapX + d.containerDim[0] * 10 / 2) + " " + (snapY + d.containerDim[1] * 10 / 2) + "),translate(" + snapX + "," + snapY + ")")
+                        .classed("normal", checkPositioning(this, d))
+                        .classed("legit", checkPositioning(this, d))
+                        .classed("adjacent", checkPositioning(this, d))
+                        .classed("overlapping", checkPositioning(this, d));
                 }
+            }
+
+            function checkPositioning(unplacedRoom, d) {
+                console.dir(unplacedRoom);
+                console.dir(d);
+                console.dir(roomPaths.data()); // objects of each room
+                console.log("roomPaths", roomPaths); // objects of each room
             }
 
             function dragended(d) {
                 if (d.notdraggable != true) {
-                    // d.boardPosition[0] = d.px = Math.round(d.boardPosition[0] / 100) * 100;
-                    // d.boardPosition[1] = d.py = Math.round(d.boardPosition[1] / 100) * 100;
                     d3.select(this).classed("dragging", false);
                 }
             }
@@ -180,7 +230,6 @@ app.directive('gridtesting', function($window) {
             function zooming() {
                 if (!d3.event.sourceEvent) return;
                 gameGrid.attr("transform", "translate(" + d3.event.translate[0] % (10 * d3.event.scale) + "," + d3.event.translate[1] % (10 * d3.event.scale) + ")scale(" + d3.event.scale + ")");
-
                 castle.attr("transform", "translate(" + d3.event.translate + ")scale(" + d3.event.scale + ")");
             }
         }
