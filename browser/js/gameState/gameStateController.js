320e6b89c9af004b1697e358106e68c2ffcdaa8b
johnquiwa <jcq211@nyu.edu>
2016-03-28 16:20:13 -0400

    playerScoring directives

--- browser/js/gameState/gameStateController.js
+++ browser/js/gameState/gameStateController.js
@@ -1,21 +1,18 @@
-app.controller('gameStats', function(kingsFavorsFactory, syncObject, $scope, $firebaseObject, gameFactory, gameStateFactory, BonusModalFactory, userId) {
+app.controller('gameStats', function(marketFactory, kingsFavorsFactory, syncObject, $scope, $firebaseObject, gameFactory, gameStateFactory, BonusModalFactory, userId) {
   var numberPlayers;
   var currentPlayer;
   var masterBuilder;
 
   $scope.noWrapSlides = false;
   $scope.active = 0;
-  var slides = $scope.slides = [{
-      image: '',
-      text: ['Nice image','Awesome photograph','That is so cool','I love that'],
-      id: 0
-    }];
-  var currIndex = 0;
+  $scope.userId = userId;
 
   syncObject.$bindTo($scope, "data")
     .then(function() {
       $scope.userIndex = gameStateFactory.getUserIndex($scope.data);
       $scope.userObj = gameStateFactory.getUserObj($scope.data);
+      $scope.players = $scope.data.players;
+      console.log($scope.data.currentPlayer);
       return $scope.userIndex;
     }).then(function(index){
       if($scope.data.turnCount === index && !$scope.data.players[index].bonusCards) {
