fd81cc53d1907b48bf72bbe194debfb3a3ac4988
Cindy Li <il.ydnic@gmail.com>
2016-03-31 12:35:09 -0400

    one player

--- browser/js/dashboard/dashboard.factory.js
+++ browser/js/dashboard/dashboard.factory.js
@@ -90,6 +90,8 @@ app.factory('DashboardFactory', function($state, gameFactory, $firebaseArray, $q
             numRoomCards = 33;
             numTileMult = -1;
             game.kingsFavors = null;
+            game.masterBuilder = null;
+            game.players[0].canBuy = true;
             game.market = {
                 2000: {room:'empty'},
                 4000: {room:'empty'},
